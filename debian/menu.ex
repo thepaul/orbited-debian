e692704fcd36d6ad3fb875bbc632a990c5adffcd
paul cannon <paul@riptano.com>
2010-11-17 16:40:17 -0700

    first pass of basic packaging

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(orbited):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="orbited" command="/usr/bin/orbited"
--- debian/orbited.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the orbited package
-#
-0 4	* * *	root	[ -x /usr/bin/orbited_maintenance ] && /usr/bin/orbited_maintenance
